6d769b549e3ed80e2d5e2e89327208c25e8b458c
Jetson16gb <robin02@outlook.de>
2024-09-25 13:49:11 +0200

    add moveit package and do first structure

--- Dockerfile
+++ Dockerfile
@@ -21,17 +21,87 @@ ENV USER=$USER
 RUN groupadd -g $GID $USER \
     && useradd -m -u $UID -g $GID --shell $(which bash) $USER
 
-#install xacro and joint state publisher gui package (additional necessarity when not using the ur-package from ros)
-USER root
+# Setup workpace
+USER $USER
+RUN mkdir -p /home/$USER/ros2_ws/src
+WORKDIR /home/$USER/ros2_ws
+
+
+# install common ros2 pkgs USER root
 RUN apt-get update && apt-get install -y \
     ros-${ROS_DISTRO}-xacro \
     ros-${ROS_DISTRO}-joint-state-publisher-gui \
     ros-${ROS_DISTRO}-rviz2
 USER ${USER}
 
-# Setup workpace
+
+# install necessary packages for ros2 control
+USER root
+RUN apt-get update && apt-get install -y ros-${ROS_DISTRO}-controller-interface \
+    ros-${ROS_DISTRO}-controller-manager \
+    ros-${ROS_DISTRO}-hardware-interface \
+    ros-${ROS_DISTRO}-pluginlib \
+    ros-${ROS_DISTRO}-rclcpp \
+    ros-${ROS_DISTRO}-rclcpp-lifecycle \
+    ros-${ROS_DISTRO}-ros2-control \
+    ros-${ROS_DISTRO}-ros2-controllers \
+    ros-${ROS_DISTRO}-rosidl-default-generators \
+    ros-${ROS_DISTRO}-std-srvs
 USER $USER
-RUN mkdir -p /home/$USER/ros2_ws/src
-WORKDIR /home/$USER/ros2_ws
+
+
+# install necessary packages for moveit
+USER root
+RUN DEBIAN_FRONTEND=noninteractive \
+	apt update && apt install -y  \
+    ros-${ROS_DISTRO}-moveit  \
+    ros-${ROS_DISTRO}-moveit-common  \
+    ros-${ROS_DISTRO}-moveit-servo  \
+    ros-${ROS_DISTRO}-moveit-ros-perception  \
+    ros-${ROS_DISTRO}-joint-trajectory-controller  \
+    ros-${ROS_DISTRO}-joint-state-broadcaster  \
+    ros-${ROS_DISTRO}-controller-manager \
+    ros-${ROS_DISTRO}-sensor-msgs-py  \
+    ros-${ROS_DISTRO}-rqt-controller-manager
+USER $USER
+
+
+
+#install dependencies to calculate with affine transformations
+USER root
+RUN DEBIAN_FRONTEND=noninteractive \
+    apt update && apt install -y  \
+    ros-${ROS_DISTRO}-tf-transformations
+
+RUN apt-get update && apt-get install -y pip
+RUN apt-get update && apt-get install -y libnlopt*
+
+RUN pip install transforms3d
+RUN pip install scipy
+USER $USER
+
+
+#install dependencies for python interface
+USER root
+RUN apt-get update && apt-get install -y pip
+    # to show usb devices with lsusb
+RUN apt-get update && apt-get install -y usbutils  
+    # to configure the CAN interface
+RUN apt-get update && apt-get install -y iproute2
+USER $USER 
+
+
+# Copy src into src folder to build the workspace initially --> mounting overwrites this
+COPY ./src /home/$USER/ros2_ws/src
+
+# Build the workspace packages
+RUN cd /home/$USER/ros2_ws && \
+     . /opt/ros/$ROS_DISTRO/setup.sh && \
+     colcon build
+
+# Add built package to entrypoint by calling install/setup.bash
+USER root
+RUN sed -i 's|exec "\$@"|source "/home/'"${USER}"'/ros2_ws/install/setup.bash"\n&|' /ros_entrypoint.sh
+USER $USER
 
 
---
+++ src/fanuc_m16ib_moveit_config/launch/moveit.launch.py
@@ -0,0 +1,252 @@
+import os
+import yaml
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_param_builder import load_yaml
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ParameterValue
+
+def load_yaml_internal(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path) as file:
+            return yaml.safe_load(file)
+    except OSError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+def opaque_test(context, *args, **kwargs):
+    tf_prefix = LaunchConfiguration("tf_prefix")
+    generate_ros2_control_tag = LaunchConfiguration("generate_ros2_control_tag")
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
+    launch_rviz  = LaunchConfiguration("launch_rviz")
+
+
+    moveit_package = "fanuc_m16ib_moveit_config"
+    description_package = "fanuc_m16ib_description"
+    
+
+
+    robot_description_file = PathJoinSubstitution(
+        [
+            FindPackageShare(description_package),
+            "urdf",
+            "fanuc_m16ib_model.urdf.xacro",
+        ]
+    )
+    robot_description = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            robot_description_file,
+            " ",
+            "tf_prefix:=",
+            tf_prefix,
+            " ",
+            "use_mock_hardware:=",
+            use_mock_hardware,
+            " ",
+            "generate_ros2_control_tag:=",
+            generate_ros2_control_tag,
+        ]
+    )
+    robot_description_semantic_file = PathJoinSubstitution(
+        [
+            FindPackageShare(moveit_package),
+            "srdf",
+            "fanuc_m16ib.srdf.xacro",
+        ]
+    )
+    robot_description_semantic = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            robot_description_semantic_file,
+            " ",
+            "tf_prefix:=",
+            tf_prefix,
+        ]
+    )
+
+    #### load configs and dependencies for movegroup node and wrapper ####
+
+    # controller/ action server for trajectory execution
+    controllers_file = os.path.join(
+        get_package_share_directory(moveit_package),
+        "config",
+        "controllers.yaml",
+    )
+
+    controllers_dict = load_yaml(Path(controllers_file.perform(context)))
+
+    moveit_controllers = {
+        "moveit_simple_controller_manager": controllers_dict,
+        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
+    }
+
+    # ik-solver
+    robot_description_kinematics_file = PathJoinSubstitution(
+        [
+            FindPackageShare(moveit_package),
+            "config",
+            "kinematics.yaml",
+        ]
+    )
+
+    # planning pipeline (algorithms and settings)
+    planning_pipeline = {
+        "move_group": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+            "start_state_max_bounds_error": 0.1,
+        },
+    }
+
+    ompl_planning_yaml = load_yaml_internal(moveit_package, "config/ompl.yaml")
+    planning_pipeline["move_group"].update(ompl_planning_yaml)
+
+    planning_scene_monitor_parameters = {
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+    }
+
+    planning_group = {"planning_group": "fanuc_m16ib"}
+    
+
+    # Octomap/ Moveit Perception setup
+    # octomap_config = {'octomap_frame': 'igus_camera_link_optical', 
+    #                   'octomap_resolution': 0.05,
+    #                   'max_range': 3.0}
+
+    # octomap_updater_file = PathJoinSubstitution(
+    #     [
+    #         FindPackageShare(moveit_package),
+    #         "config",
+    #         "sensor_pointcloud.yaml",
+    #     ]
+    # )
+    # octomap_updater_config = load_yaml(Path(octomap_updater_file.perform(context)))
+
+    moveit_args_not_concatenated = [
+        {"robot_description": robot_description.perform(context)},
+        {"robot_description_semantic": robot_description_semantic.perform(context)},
+        moveit_controllers,
+        planning_scene_monitor_parameters,
+        planning_pipeline,
+        {
+            "publish_robot_description": True,
+            "publish_robot_description_semantic": True,
+            "publish_geometry_updates": True,
+            "publish_state_updates": True,
+            "publish_transforms_updates": True,
+        },
+        planning_pipeline,
+        #octomap_config,
+        #octomap_updater_config
+    ]
+
+    # Concatenate all dictionaries together, else moveitpy won't read all parameters
+    moveit_args = dict()
+    for d in moveit_args_not_concatenated:
+        moveit_args.update(d)
+
+
+
+    #### Nodes to launch ####
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        parameters=[
+            moveit_args,
+            robot_description_kinematics_file,
+        ],
+    )
+
+    moveit_wrapper_node = Node(
+        package="moveit_wrapper",
+        executable="moveit_wrapper_node",
+        output="screen",
+        parameters=[robot_description, robot_description_semantic, robot_description_kinematics_file, planning_group],
+    )
+
+    # rviz with moveit configuration
+    #rviz_config_file = PathJoinSubstitution([FindPackageShare(moveit_package), "rviz", "rviz_config.rviz"]) # define path to rviz-config file
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        condition= IfCondition(launch_rviz),
+        parameters=[
+            robot_description_kinematics_file,  # needed for interactive marker on the tcp
+        ],
+        #arguments=["-d", rviz_config_file],
+    )
+
+    # nodes to start
+    return [
+        move_group_node,
+        moveit_wrapper_node,
+        rviz_node,
+    ]
+
+
+
+
+def generate_launch_description():
+#declare launch arguments (can be passed in the command line while launching)
+
+    
+    tf_prefix_arg = DeclareLaunchArgument(
+            "tf_prefix",
+            default_value="fanuc_m16ib_"
+        )
+
+    use_mock_hardware_arg = DeclareLaunchArgument(
+            "use_mock_hardware",
+            default_value="true",
+            description="start the robot with fake(mock) hardware or real controller",
+        )
+    
+    generate_ros2_control_tag_arg = DeclareLaunchArgument(
+            "generate_ros2_control_tag",
+            default_value="false",
+            description="enable ros2 control for hardware interface (realtime control, mock, simulated or real hardware)",
+        )
+
+    launch_rviz_arg = DeclareLaunchArgument(
+            "launch_rviz",
+            default_value="true",
+            choices=["false", "true"],
+            description="Whether to start rviz with the launch file or not",
+        )
+
+
+    ld = LaunchDescription()
+
+    ld.add_action(use_mock_hardware_arg)
+    ld.add_action(tf_prefix_arg)
+    ld.add_action(generate_ros2_control_tag_arg)
+    ld.add_action(launch_rviz_arg)
+
+    ld.add_action(OpaqueFunction(function=opaque_test))
+
+    return ld
