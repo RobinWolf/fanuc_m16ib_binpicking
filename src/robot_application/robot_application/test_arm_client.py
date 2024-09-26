50efd8aceb1737415b8600156ecb4baa4db17fe8
Jetson16gb <robin02@outlook.de>
2024-09-26 10:45:40 +0200

    add coupling of axis 2 and 3

--- src/robot_application/robot_application/test_arm_client.py
+++ src/robot_application/robot_application/test_arm_client.py
@@ -16,26 +16,29 @@ def main():
 
     #define a home position (when want to use default [0.0, 0.0, 0.0, 0.0, 0.0, 0.0] you don't need this definition) -> floats required
     robot.home_position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    robot.setVelocity(0.5)
+    robot.setVelocity(0.2)
 
     # move robot to home position
     robot.home()
 
-    home_affine = robot.get_transform('fanuc_m16ib_tool0', 'fanuc_m16ib_base_link', affine=True)
-    print(f'home_affine: {home_affine}')
- 
-    # move robot to a specific position
-    test_affine = Affine((0.653, 1.234, 0.766), (0.241, 0.969, 0.038 ,0.009))
-    print('Test Affine:', test_affine)
-    print('Test_Affine_Translation:', test_affine.translation)
-    print('Test_Affine_Quaternion:', test_affine.quat)
-
-    feedback = robot.ptp(test_affine)
+    for i in range(10):
+        home_affine = robot.get_transform('fanuc_m16ib_tool0', 'fanuc_m16ib_base_link', affine=True)
+        print(f'home_affine: {home_affine}')
     
-    print('Feedback:', feedback)
+        # move robot to a specific position
+        test_affine = Affine((0.653, 1.234, 0.766), (0.241, 0.969, 0.038 ,0.009))
+        print('Test Affine:', test_affine)
+        print('Test_Affine_Translation:', test_affine.translation)
+        print('Test_Affine_Quaternion:', test_affine.quat)
 
-    # move robot to home position
-    robot.home()
+        feedback = robot.ptp(test_affine)
+        
+        print('Feedback:', feedback)
+
+        # move robot to home position
+        robot.home()
+
+
     # destroy the robot node, stop execution
     robot.destroy_node()
 
