75abde87247d6aec6fbee4a022c1fb8d98ca62e0
Jetson16gb <robin02@outlook.de>
2024-09-27 11:05:47 +0200

    kinematics tuner

--- src/robot_application/robot_application/test_arm_client.py
+++ src/robot_application/robot_application/test_arm_client.py
@@ -16,7 +16,7 @@ def main():
 
     #define a home position (due to linked joint 2 and 3, there is an additional virtual joint (i=2) which has to be 0.0 all the time !!!
     robot.home_position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    robot.setVelocity(0.2)
+    robot.setVelocity(0.5)
 
     # move robot to home position
     robot.home()
@@ -24,7 +24,7 @@ def main():
     home_affine = robot.get_transform('fanuc_m16ib_tool0', 'fanuc_m16ib_base_link', affine=True)
     print(f'home_affine: {home_affine}')
 
-    for i in range(30):
+    for i in range(100):
         # move robot to pre-pick
         prepick_affine = Affine((-0.0009161331690847874, 1.0161885738372803, 0.8262154459953308), (0.0001285069592995569, 0.9999606013298035, 0.008876869454979897, -3.314939749543555e-05))
         feedback = robot.ptp(prepick_affine)
@@ -45,6 +45,8 @@ def main():
         feedback_home = robot.home()
         print('Motion Feedback Home:', feedback_home)
 
+    # move robot to home position
+    feedback_home = robot.home()
 
     # destroy the robot node, stop execution
     robot.destroy_node()
